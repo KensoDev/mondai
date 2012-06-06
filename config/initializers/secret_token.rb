9a8fa1ea67b87e61c23124ba0ff8892588e0386d
Avi Tzurel <avi@kensodev.com>
2012-06-06 16:34:21 +0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mondai::Application.config.secret_token = '2d823d22549ff2355a80d5072a22e9836d3605969f557c25b3aed61439d9a8ae4e53611377b88a104543d5f9796494faedd1d62dd9fee4dc208f22fd58b1ab7d'
